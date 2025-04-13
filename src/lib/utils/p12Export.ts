0c4cd197dea3d02bb476caaff0da35dac66c3ec4
hongzhi0123 <55428217+hongzhi0123@users.noreply.github.com>
2025-04-13 15:08:13 +0200

    Add chain of CAs into p12

--- src/lib/utils/p12Export.ts
+++ src/lib/utils/p12Export.ts
@@ -1,25 +1,27 @@
-import froge from 'node-forge';
+import forge from 'node-forge';
 
 export function createP12(
     certificatePem: string,
     privateKeyPem: string,
-    password: string
+    password: string,
+    caCertificatesPem: string[], // Array of CA certificates in PEM format
 ): Buffer {
 
     // Convert PEM to Forge objects
-    const privateKey = froge.pki.privateKeyFromPem(privateKeyPem);
-    const certificate = froge.pki.certificateFromPem(certificatePem);
+    const privateKey = forge.pki.privateKeyFromPem(privateKeyPem);
+    const certificate = forge.pki.certificateFromPem(certificatePem);
+    const caCertificates = caCertificatesPem.map(caPem => forge.pki.certificateFromPem(caPem));
 
     // Create a PKCS#12 structure
-    const p12Asn1 = froge.pkcs12.toPkcs12Asn1(
+    const p12Asn1 = forge.pkcs12.toPkcs12Asn1(
         privateKey,
-        certificate,
+        [certificate,...caCertificates], // Add CA certificates
         password,   // Set password for the PKCS#12 file
-        { algorithm: '3des' }   // Use 3DES encryption
+        { algorithm: '3des' },   // Use 3DES encryption
     );
 
     // Convert the ASN.1 structure to DER format
-    const p12Der = froge.asn1.toDer(p12Asn1).getBytes();
+    const p12Der = forge.asn1.toDer(p12Asn1).getBytes();
     // Convert the DER format to a Buffer
     return Buffer.from(p12Der, 'binary');
 }
--- src/routes/api/certificates/+server.js
+++ src/routes/api/certificates/+server.js
@@ -28,7 +28,7 @@ export async function POST({ request }) {
     await certStore.add(newCertData);
 
     // Generate the PKCS#12 file
-    const p12Buffer = createP12(newCertData.cert, newCertData.key, 'changeit');
+    const p12Buffer = createP12(newCertData.cert, newCertData.key, 'changeit', [caCert.cert.toString("pem")]);
 
     return new Response(p12Buffer, {
         headers: {
